fc84560439f1c552604ae75a468362b05d9b3cb3
Ruby Wolf <ruby.w@pm.me>
2019-08-03 23:42:01 +0000

    Database table fixes.

--- module.php
+++ module.php
@@ -1,21 +1,21 @@
 <?php
 
 class TaskTrackerModule extends OBFModule {
-  
+
   public $name        = "Task Tracker v0.1";
   public $description = "Tracks tasks with assigned playlists and media items.";
-  
+
   public function callbacks () {
-    
+
   }
-  
+
   public function install () {
     $this->db->insert('users_permissions', [
       'category'    => 'task tracker',
       'description' => 'manage tasks',
       'name'        => 'task_tracker_module_manage'
     ]);
-    
+
     $this->db->query('CREATE TABLE IF NOT EXISTS `module_task_tracker` (
       `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
       `name` varchar(255) NOT NULL,
@@ -24,29 +24,29 @@ class TaskTrackerModule extends OBFModule {
       `due` int(11) unsigned,
       `status` enum("new", "in progress", "complete") default "new" NOT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
-    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
     $this->db->query('CREATE TABLE IF NOT EXISTS `module_task_tracker_media` (
       `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
       `task_id` int(10) unsigned NOT NULL,
       `media_id` int(11) unsigned NOT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
-    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
     $this->db->query('CREATE TABLE IF NOT EXISTS `module_task_tracker_playlists` (
       `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
       `task_id` int(10) unsigned NOT NULL,
       `playlist_id` int(11) unsigned NOT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
-    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
     $this->db->query('CREATE TABLE IF NOT EXISTS `module_task_tracker_users` (
       `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
       `task_id` int(10) unsigned NOT NULL,
       `user_id` int(11) unsigned NOT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
-    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
     $this->db->query('CREATE TABLE IF NOT EXISTS
       `module_task_tracker_comments` (
       `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -55,11 +55,11 @@ class TaskTrackerModule extends OBFModule {
       `comment` text,
       `created` int(11) unsigned NOT NULL,
       PRIMARY KEY (`id`)
-    ) ENGINE MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;');
-    
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+
     return true;
   }
-  
+
   public function uninstall () {
     $this->db->where('name','task_tracker_module_manage');
     $permission = $this->db->get_one('users_permissions');
@@ -69,10 +69,10 @@ class TaskTrackerModule extends OBFModule {
 
     $this->db->where('id', $permission['id']);
     $this->db->delete('users_permissions');
-    
+
     // Keep tables for now.
     // $this->db->query('DROP TABLE `module_task_tracker`');
-    
+
     return true;
   }
 }
